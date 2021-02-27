df9b989cbeea79bea35edcc27fc96b7eb33df6e5
Giacomo Gonzato <75306061+GiacomoGonzato@users.noreply.github.com>
2021-02-27 02:05:10 +0100

    Correzione errore plancia trasposta in ai_tris

--- utils/ai_tris.py
+++ utils/ai_tris.py
@@ -14,7 +14,7 @@ def check_mosse_possibili(board):
 def valore_mossa(board, contatore, alpha=- inf, beta=inf):
     tavolo = plancia()
     tavolo.get_plancia = [[board.get_plancia[i][j]
-                           for i in range(3)] for j in range(3)]
+                           for j in range(3)] for i in range(3)]
 
     check_win = tavolo.check_vittoria()
     if check_win == 1:
@@ -29,7 +29,7 @@ def valore_mossa(board, contatore, alpha=- inf, beta=inf):
         for mossa in check_mosse_possibili(tavolo):
             banco_prova = plancia()
             banco_prova.get_plancia = [[board.get_plancia[i][j]
-                                        for i in range(3)] for j in range(3)]
+                                        for j in range(3)] for i in range(3)]
             banco_prova.posiziona(mossa, 'X')
             valore = max(valore, valore_mossa(
                 banco_prova, contatore + 1, alpha, beta))
@@ -43,7 +43,7 @@ def valore_mossa(board, contatore, alpha=- inf, beta=inf):
         for mossa in check_mosse_possibili(tavolo):
             banco_prova = plancia()
             banco_prova.get_plancia = [[board.get_plancia[i][j]
-                                        for i in range(3)] for j in range(3)]
+                                        for j in range(3)] for i in range(3)]
             banco_prova.posiziona(mossa, 'O')
             valore = min(valore, valore_mossa(
                 banco_prova, contatore + 1, alpha, beta))
